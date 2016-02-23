c6e762ff50db13ace493af2b9c17a49b3dc8f9eb
Anna Moyers <annamoyers@FeeFiFoFum.local>
2016-02-22 17:15:38 -0700

    Includes index.php for our homepage, a generic PHP template, page_template to build pages in the future.  If we don't want the page to be PHP, change the filetype.  I am going to update this to include necessary headers and footers for formatting.  Made folder and stylesheet necessary to add in dedicated CSS.

---
+++ page_template.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+
+<!-------------------------------------------------------------------->
+<!-- If re-used as a general HTML template, rename filetype to .HTML-->
+<!-------------------------------------------------------------------->
+
+<?php
+    session_start();
+
+?>
+
+<html>
+<head>
+    </head>
+<body>
+    </body>
+
+
+</html>
