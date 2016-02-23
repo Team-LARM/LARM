c5c3dff10b8a1adf4dbf1fcf960be37570c01cdd
Anna Moyers <annamoyers@FeeFiFoFum.local>
2016-02-23 10:45:44 -0700

    added additional templating to general site template

--- page_template.php
+++ page_template.php
@@ -4,16 +4,38 @@
 <!-- If re-used as a general HTML template, rename filetype to .HTML-->
 <!-------------------------------------------------------------------->
 
+
+<!--Remove PHP tags as needed-->
 <?php
     session_start();
-
 ?>
+
 
 <html>
 <head>
+    <title>*NAME HERE*</title>
+    <link href="css/styles.css" type="text/css" rel="stylesheet" />
     </head>
+
 <body>
+    <div class="pageContainer">
+    
+        </div>
+    
+    <div class="headerContainer">
+    
+        </div>
+    
+    <div class="bodyContainer">
+    
+        </div>
+    
+    <div class="footerContainer">
+    
+        </div>
+    
     </body>
-
-
+    
+    
 </html>
+
