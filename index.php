3af5140187bb5e68cf7bc8993ba92a60095b3c24
Anna Moyers <annamoyers@FeeFiFoFum.local>
2016-02-23 10:44:58 -0700

    Adding basic structure of CSS into pages

--- index.php
+++ index.php
@@ -6,9 +6,30 @@
 
 <html>
 <head>
+    <title>*NAME HERE*</title>
+    <link href="css/styles.css" type="text/css" rel="stylesheet" />
     </head>
+
 <body>
+    <div class="pageContainer">
+    
+    </div>
+    
+    <div class="headerContainer">
+    
+    </div>
+    
+    <div class="bodyContainer">
+    
+    </div>
+    
+    <div class="footerContainer">
+    
+    </div>
+    
     </body>
+    
+    
 
 
 </html>
